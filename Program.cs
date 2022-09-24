d6f9d759b39bd65b8a2071f3a5eace717cbb6ed4
Rafael <faelmota7@gmail.com>
2022-09-23 22:40:16 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+int numero, face;
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("\n--- Rolagem de Dado ---");
+
+Console.ResetColor();
+
+Console.Write("Quantas faces ?: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+face = RandomNumberGenerator.GetInt32(1,++numero);
+
+Console.Write("\nRolagem: ");
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine($"{face}\n");
+
+Console.ResetColor();
+
+
